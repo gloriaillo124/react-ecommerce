7e45cf38c6044bd930d2d0a70e35969ddb16c25a
gloriaillo124 <gloriaillo124@gmail.com>
2024-01-12 01:31:25 +0100

    premier commit ,creation header et container

---
+++ src/composant/Header/Header.js
@@ -0,0 +1,15 @@
+
+import './Header.css';
+
+var Header = () =>{
+            return <header>
+                    <nav>
+                        <div className="nav-item">Accueil</div>
+                        <div className="nav-item">A propos</div>
+                        <div className="nav-item">Boutique</div>
+                        <div className="nav-item">Contact</div>
+                    </nav>
+                </header>
+}
+
+export default Header
